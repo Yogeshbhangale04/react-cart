4dd437f29b457fc03e8935489b56226141dad215
Yogesh Bhangale <yogesh.bhangale04@gmail.com>
2024-07-19 00:52:04 +0530

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,27 @@
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyH7Xeaeqi3h7Ws3XWhQRIxrLD21B-SJHwHgLCWSE38gihSj9qa_KNRuDx86E5hKx3RHn6_PrBU6Y0tDE9Tm8SiS1IdPy4SZP6t99bCgI1AqxXV8-dG2fZGw&usqp=CAE",
+            title: "Titan 1806NM01 - Watch for Men",
+            desc: "Model: CLW001 Brand, Manufacture, Warranty by WatchMaster. Classic leather band, water-resistant, elegant design.",
+            price: "4599",
+        },
+        {
+            id: 2,
+            img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQjBZg6Y6jUXoWHcp3rusEeQg_sv23gn6FxTdfX4Mgl8al-kpT7JjF7lYNe-6q_oplfta1wOu2_AiQ6dCg4LutoPta-OtLjPp06CypvBfc&usqp=CAE",
+            title: "Formal Watch",
+            desc: "Model: SPW002 Brand, Manufacture, Warranty by WatchMaster. Durable and stylish, perfect for sports and outdoor activities.",
+            price: "3299",
+        },
+        {
+            id: 3,
+            img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS58KWsRtygHb2c6UF91zRAJ4-mHh-YhtMG5q3DIKPnOO9WL44vibIFX-VTxKXyG1f1iOGTYhWckjmRvkjg58cK-H7zKEsL9fOvb8-7m-xd0gVDCcoxQiFvaw&usqp=CAE",
+            title: "Titan Edge Ceramic Quartz in Glossy Green Dial Watch for Men",
+            desc: "Model: EGW003 Brand, Manufacture, Warranty by WatchMaster. Luxurious gold-plated, sophisticated design.",
+            price: "7899",
+        }
+    ]
+}
+
+export default data;
